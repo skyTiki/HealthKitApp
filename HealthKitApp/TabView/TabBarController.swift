a819eacb14eb3567aa6de1873bd6a05869cde8cd
takatoshi.ichige <tichige@j-skk.com>
2021-11-10 14:25:58 +0900

    画面構成だけ作成

---
+++ HealthKitApp/TabView/TabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarController.swift
+//  HealthKitApp
+//
+//  Created by S-wayMock2 on 2021/11/10.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+}
+
--- HealthKitApp/Views/Daily/DailyStepViewController.swift
+++ HealthKitApp/Views/Daily/DailyStepViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DailyStepViewController.swift
 //  HealthKitApp
 //
 //  Created by S-wayMock2 on 2021/11/10.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class DailyStepViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
