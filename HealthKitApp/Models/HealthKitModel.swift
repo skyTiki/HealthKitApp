79832c4d9e9b1e350d5392db3f4b7825cfe4a888
takatoshi.ichige <tichige@j-skk.com>
2021-11-10 15:21:02 +0900

    歩数の取得処理実装

---
+++ HealthKitApp/Models/HealthKitModel.swift
@@ -0,0 +1,67 @@
+//
+//  HealthKitModel.swift
+//  HealthKitApp
+//
+//  Created by S-wayMock2 on 2021/11/10.
+//
+
+import Foundation
+import HealthKit
+
+class HealthKitModel {
+    
+    static let healthStore = HKHealthStore()
+    
+    static var readHealthDataTypes: Set<HKSampleType> {
+        [
+            // 必要に応じて追加
+            HKQuantityType(.stepCount)
+        ]
+    }
+    
+    
+    static func requestAuthorization(completion: @escaping(_ success: Bool) -> Void) {
+        
+        // デバイスが使用可能かどうか
+        // TODO: fatalErrorではなくダイアログとかにする
+        if !HKHealthStore.isHealthDataAvailable() { fatalError("デバイスが対応していません") }
+        
+        // ユーザーに許可を得る
+        healthStore.requestAuthorization(toShare: [], read: readHealthDataTypes) { success, error in
+            if let error = error {
+                print("requestAuthorization error:", error.localizedDescription)
+            }
+            
+            print(success ? "requestAuthorization was success" : "requestAuthorization was not success")
+            
+            completion(success)
+            
+        }
+        
+    }
+    
+    static func getHealthKitStepCount(withStart: Date, end: Date, completion: @escaping(_ results : [HKSample]) -> Void) {
+        
+        print(withStart,"~", end,"までの歩数データを取得します")
+        
+        let predicate = HKQuery.predicateForSamples(withStart: withStart, end: end, options: [])
+        // 歩数カウント
+        let queryDescriptor: [HKQueryDescriptor] = [HKQueryDescriptor(sampleType: HKQuantityType(.stepCount), predicate: predicate)]
+                                                
+        let query = HKSampleQuery(queryDescriptors: queryDescriptor, limit: HKObjectQueryNoLimit, sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: true)]) { query, results, error in
+            if let error = error {
+                fatalError("HKSampleQuery error:\(error.localizedDescription)")
+            }
+            
+            if let results = results {
+                completion(results)
+            } else {
+                fatalError("HKSampleQuery results was nil")
+            }
+            
+        }
+        
+        healthStore.execute(query)
+        
+    }
+}
--- HealthKitApp/Views/Daily/DailyStepViewController.swift
+++ HealthKitApp/Views/Daily/DailyStepViewController.swift
@@ -6,10 +6,55 @@
 //
 
 import UIKit
+import HealthKit
 
 class DailyStepViewController: UIViewController {
+    
+    
+    @IBOutlet weak var stepCountLabel: UILabel!
+    
+    var stepCount = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        HealthKitModel.requestAuthorization { success in
+            if success {
+                print("歩数を取得できます")
+            } else {
+                print("歩数を取得できます")
+            }
+        }
     }
+    
+    
+    @IBAction func tappedGetStepCountButton(_ sender: Any) {
+        
+        let nowDate = Date()
+        let startOfDate = Calendar.current.startOfDay(for: nowDate)
+//        let startOfDate = Calendar.current.date(byAdding: .day, value: -9, to: nowDate)!
+        
+        
+        HealthKitModel.getHealthKitStepCount(withStart: startOfDate, end: nowDate) { results in
+            guard let results = results as? [HKQuantitySample] else { return }
+            print("results", results.count , results)
+            
+            // 歩数の合計
+            var stepCountSum = 0.0
+            for num in 0..<results.count {
+                stepCountSum += results[num].quantity.doubleValue(for: .count())
+            }
+            
+            self.stepCount = stepCountSum
+             
+            // 画面に反映
+            DispatchQueue.main.async {
+                self.stepCountLabel.text = "\(self.stepCount)"
+            }
+        }
+        
+        
+    }
+    
 }
 
