477bdac10e4c1791b8700916d8ebb4079f6f6841
takatoshi.ichige <tichige@j-skk.com>
2021-11-15 11:59:21 +0900

    ChartViewの表示

--- HealthKitApp/Views/Weekly/WeeklyStepViewController.swift
+++ HealthKitApp/Views/Weekly/WeeklyStepViewController.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import Charts
 import HealthKit
 
 class WeeklyStepViewController: UIViewController {
     
+    @IBOutlet weak var lineChartView: LineChartView!
+    
     var stepWeeklyCount = 0.0
+    
+    var entries: [ChartDataEntry] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +28,28 @@ class WeeklyStepViewController: UIViewController {
         HealthData.getStepCountSumPerDay(withStart: startDate, end: nowDate) { results in
             print("実行完了")
             results.forEach { print("step", $0.sumQuantity()!) }
+            
+            DispatchQueue.main.async {
+                
+                self.setChartViewData(data: results)
+                self.updateCharView()
+            }
         }
+    }
+    
+    
+    private func setChartViewData(data: [HKStatistics]) {
+        
+        for (index, statistic) in data.enumerated() {
+            self.entries.append(ChartDataEntry(x: Double(index), y: statistic.sumQuantity()!.doubleValue(for: .count())))
+        }
+    }
+    
+    
+    private func updateCharView() {
+        let chartDataSet = LineChartDataSet(entries: entries, label: "歩数")
+        
+        lineChartView.data = LineChartData(dataSet: chartDataSet)
+        
     }
 }
